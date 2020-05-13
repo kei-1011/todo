e9f8cb36963c936de83cdabe87511bcba68e2513
kohei <k_kojima0407@yahoo.co.jp>
2020-05-13 09:31:53 +0900

    フォルダ一覧のカレント表示修正

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ define('DSN', 'mysql:dbname=todo_app;host=localhost;charset=utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 
-define('SITE_URL', 'http://'. $_SERVER['HTTP_HOST']);
+define('SITE_URL', 'http://'. $_SERVER['HTTP_HOST'].'/');
 // DB接続情報
 try {
   $dbh = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
--- lib/view/folders.php
+++ lib/view/folders.php
@@ -1,11 +1,10 @@
 <?php
-$url = $_SERVER['REQUEST_URI'];
-
+$url = 'http://'. $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
 <ul class="folder-list">
-    <li class="folder-item"><a href="index.php" class="folder-link full <?php if($url === '/index.php'){ echo "current";}?>">全てのフォルダ</a></li>
-    <li class="folder-item"><a href="done.php" class="folder-link full-done <?php if($url === '/done.php'){ echo "current";}?>">完了済のタスク</a></li>
+    <li class="folder-item"><a href="<?php echo SITE_URL;?>" class="folder-link full <?php if($url === SITE_URL){ echo "current";}?>">全てのフォルダ</a></li>
+    <li class="folder-item"><a href="done.php" class="folder-link full-done <?php if($url === SITE_URL.'done.php'){ echo "current";}?>">完了済のタスク</a></li>
     <?php foreach($folders as $folder){?>
-      <li class="folder-item"><a href="index.php?folder_id=<?php echo $folder['id'];?>" class="folder-link <?php if($folder_id == $folder['id']) { echo 'current'; }?>"><?php echo $folder['title'];?></a><a href="update_folder.php?folder_id=<?php echo $folder['id'];?>" class="folder-update">編集</a></li>
+      <li class="folder-item"><a href="index.php?folder_id=<?php echo $folder['id'];?>" class="folder-link <?php if($_GET['folder_id'] == $folder['id']) { echo 'current'; }?>"><?php echo $folder['title'];?></a><a href="update_folder.php?folder_id=<?php echo $folder['id'];?>" class="folder-update">編集</a></li>
     <?php }?>
     </ul>
