70297b8db36b2fbb0830b28812ca9bad87c29541
kohei <k_kojima0407@yahoo.co.jp>
2020-05-13 11:47:23 +0900

    js fix Please enter the commit message for your changes. Lines starting

--- public_html/src/js/script.js
+++ public_html/src/js/script.js
@@ -2,17 +2,4 @@ $(function () {
   $('#delete_btn').on('click', function () {
     return confirm("削除してもよろしいですか？");
   });
-
-  $('#js-add_todo').on('click', function () {
-    $('body').addClass('window-up');
-  });
-  $('.close-modal').on('click', function () {
-    $('body').removeClass('window-up');
-  });
-
-  $(document).on('click', function () {
-    if($(event.target).hasClass('window-overlay')) {
-      $('body').removeClass('window-up');
-    }
-  });
 });
