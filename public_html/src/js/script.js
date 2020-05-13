f730cd7962bb5e31a462a28a9078161bd465f6fc
kohei <k_kojima0407@yahoo.co.jp>
2020-05-14 00:33:39 +0900

    sidebar drawer

--- public_html/src/js/script.js
+++ public_html/src/js/script.js
@@ -2,4 +2,12 @@ $(function () {
   $('#delete_btn').on('click', function () {
     return confirm("削除してもよろしいですか？");
   });
+
+  $('#drawer').on('click', function () {
+    if ($('body').hasClass('close')) {
+      $('body').removeClass('close');
+    } else {
+      $('body').addClass('close');
+    }
+  });
 });
